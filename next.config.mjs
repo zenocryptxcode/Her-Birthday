ef3f1f82fe81b4a78b789f5b092c194732d9b5a3
zenocryptxcode <zenocryptx@gmail.com>
2025-11-24 01:49:59 +0530

    Update Next.js configuration for output and distDir

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
